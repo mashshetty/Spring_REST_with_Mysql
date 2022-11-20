cc36dbaa01a8e50b4e15c2f2d3680151475d69f2
mashshetty <vitheshshetty555@gmail.com>
2022-11-20 18:00:41 +0530

    push

--- rest/src/main/java/com/rest/RestApplication.java
+++ rest/src/main/java/com/rest/RestApplication.java
@@ -2,6 +2,11 @@ package com.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RestApplication {
@@ -9,5 +14,22 @@ public class RestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
+	
+	
 
+}
+
+@Configuration
+ class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 }
